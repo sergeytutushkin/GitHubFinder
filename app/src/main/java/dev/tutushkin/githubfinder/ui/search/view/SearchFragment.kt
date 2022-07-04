d98d5cee18ae7964e43ea5d5e66adb76b7550083
sergeytutushkin <sergeytutushkin@gmail.com>
2022-07-04 16:44:32 +0300

    ViewBinding added

--- app/src/main/java/dev/tutushkin/githubfinder/ui/search/view/SearchFragment.kt
+++ app/src/main/java/dev/tutushkin/githubfinder/ui/search/view/SearchFragment.kt
@@ -6,18 +6,28 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import dev.tutushkin.githubfinder.R
+import dev.tutushkin.githubfinder.databinding.FragmentSearchBinding
 import dev.tutushkin.githubfinder.ui.search.viewmodel.SearchViewModel
 
 @AndroidEntryPoint
 class SearchFragment : Fragment(R.layout.fragment_search) {
+
+    private var fragmentSearchBinding: FragmentSearchBinding? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val viewModel by viewModels<SearchViewModel>()
 
+        val binding = FragmentSearchBinding.bind(view)
+        fragmentSearchBinding = binding
 
-        println("aaa${viewModel.repos}")
+        binding.textView.text = viewModel.repos.toString()
+    }
+
+    override fun onDestroyView() {
+        fragmentSearchBinding = null
+        super.onDestroyView()
     }
 
 }
